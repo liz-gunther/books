b6214a20401baba613a1a73ea99afb6cb2b018a8
Liz <liz.gunther@gmail.com>
2023-01-06 14:35:31 -0600

    generate id

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { useState } from "react";
+import BookCreate from "./components/BookCreate";
+
 function App() {
-    return <div>App</div>
+
+    const [books, setBooks] = useState([]);
+
+    const createBook = (title) => {
+        const updatedBooks = [
+            ...books,
+            { 
+                id: Math.round(Math.random * 9999), 
+                title 
+            }
+        ];
+        setBooks(updatedBooks);
+    };
+
+    return <div>
+        <BookCreate onCreate={createBook}/>
+    </div>
 }
 
 export default App;
